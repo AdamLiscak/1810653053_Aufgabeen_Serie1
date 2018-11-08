96e4571fefc7ad8382390420a6339f06ccee207d
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-11-08 10:11:40 +0100

    Aufgabe3

---
+++ src/meinDrittesPacket/DritteKlasse.java
@@ -0,0 +1,8 @@
+package meinDrittesPacket;
+
+public class DritteKlasse {public static void main(String[] args){
+    String name="Graf Dracula";
+    int alter= 666;
+    System.out.println(name + " " + "ist " + alter + " jahre alt.");
+}
+}
