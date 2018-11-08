d1d48eb275be3f31b2cd905a9fb744dfa8754e1d
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-11-08 10:42:32 +0100

    Aufgabe4

---
+++ src/VierteKlasse.java
@@ -0,0 +1,13 @@
+public class VierteKlasse
+{
+    public static void main ( String [] args )
+    {
+       final String FH_KUFSTEIN_TIROL= " Fachhochschule Kufstein Tirol";
+    }
+        /* Zuerst hatte ich String fachhoschuleKufsteinTirol, dann hab ich sie über
+        RechtsClick(refactor(rename)) in fhKufsteinTirol umbenannt. Dann habe ich das Sclüsselwort final
+        vor diesem String hingegeben und die Variable wieder über rename auf FH_KUFSTEIN_TIROL umbenannt */
+
+
+
+}
