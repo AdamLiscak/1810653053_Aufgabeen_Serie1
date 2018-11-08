75337925c2a5f1b199a1f263a3dc26317fb1fb4a
Liscak Adam <1810653053@stud.fh-kufstein.ac.at>
2018-11-08 09:55:57 +0100

    Auskommentierung

--- src/meinpacket/Main.java
+++ src/meinpacket/Main.java
@@ -1,9 +1,9 @@
-package meinpacket;
-public class Main {
+package meinpacket; // Zuordnung vom Package.. ohne diese würde  der Program den default package angehören
+public class Main { // deklaration der Klasse "Main"
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");System.out.println("Adam Liscak");
-        System.out.println();
+    public static void main(String[] args) { //Deklaration der Hauptmethode
+        System.out.println("Hello World!");System.out.println("Adam Liscak"); // Ausgabe von HelloWorld und Adam Liscak
+        System.out.println(); // nichts wird ausgegeben
 
     }
 }
